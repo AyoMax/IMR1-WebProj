866b5c668dc80713e3ae09c9d922348d46a792c2
Ayolyan <yoyan.bid@gmail.com>
2020-06-21 23:13:12 +0200

    Bootstrap Template + MongoDB connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 
+// Connexion à la BDD mongodb
+const mongoose = require('mongoose');
+const mongodbUri = "mongodb+srv://ayomax:5mMwlLPIyZf3k68i@imr1webproj-ac8e2.mongodb.net/imr1webproj";
+mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Redirection de la home page
 app.get('/', (req, res) => {
